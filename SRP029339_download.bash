797393b98b090e6a3789768e54b30d686a9ce69f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-25 06:33:06 +0800

    Update SRP029339_download.bash

--- SRP029339_download.bash
+++ SRP029339_download.bash
@@ -17,3 +17,16 @@ echo $SRA;
 done < mice_biosample_result_edited.txt
 
 sudo /home/rtm/myprograms/Bismark/bismark_genome_preparation --bowtie2 /home/references/mm10_noScaffold/
+
+
+/home/rtm/myprograms/Bismark/bismark_genome_preparation --bowtie2 /home/rtm/resources/hg38_bs_quy/
+
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D0/WT_D0.Rep2.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D14/WT_D14.Rep1.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D14/WT_D14.Rep3.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D14/WT_D14.Rep4.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D6/WT_D6.Rep1.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D6/WT_D6.Rep3.read1_bismark_bt2_pe.bam
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 22 --gzip --buffer_size 150G --paired-end --ample_memory --comprehensive --cytosine_report --CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/DNA_methylation_and_gene_splicing/WGBS/bismark_alignment/WT_D6/WT_D6.Rep4.read1_bismark_bt2_pe.bam
+
+

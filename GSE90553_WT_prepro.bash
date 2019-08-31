c57e84d31f3aef079dad2dac9d2951b8ea12cc53
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-31 17:16:13 +0800

    Update GSE90553_WT_prepro.bash

--- GSE90553_WT_prepro.bash
+++ GSE90553_WT_prepro.bash
@@ -1,1 +1,26 @@
 
+cat WT_D0_R1*_1.fastq.gz > WT_D0_rep1_R1.fastq.gz
+cat WT_D0_R1*_2.fastq.gz > WT_D0_rep1_R2.fastq.gz
+
+cat WT_D0_R2*_1.fastq.gz > WT_D0_rep2_R1.fastq.gz
+cat WT_D0_R2*_2.fastq.gz > WT_D0_rep2_R2.fastq.gz
+
+mv WT_D6_R1_l1_1.fastq.gz WT_D6_rep1_R1.fastq.gz
+mv WT_D6_R1_l1_2.fastq.gz WT_D6_rep1_R2.fastq.gz
+
+cat WT_D6_R3*_1.fastq.gz > WT_D6_rep3_R1.fastq.gz
+cat WT_D6_R3*_2.fastq.gz > WT_D6_rep3_R2.fastq.gz
+
+cat WT_D6_R4*_1.fastq.gz > WT_D6_rep4_R1.fastq.gz
+cat WT_D6_R4*_2.fastq.gz > WT_D6_rep4_R2.fastq.gz
+
+mv WT_D14_R1_l1_1.fastq.gz WT_D14_rep1_R1.fastq.gz
+mv WT_D14_R1_l1_2.fastq.gz WT_D14_rep1_R2.fastq.gz
+
+cat WT_D14_R3*_1.fastq.gz > WT_D14_rep3_R1.fastq.gz
+cat WT_D14_R3*_2.fastq.gz > WT_D14_rep3_R2.fastq.gz
+
+cat WT_D14_R4*_1.fastq.gz > WT_D14_rep4_R1.fastq.gz
+cat WT_D14_R4*_2.fastq.gz > WT_D14_rep4_R2.fastq.gz
+
+#######

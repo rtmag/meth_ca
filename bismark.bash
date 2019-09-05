6876ca465a546521c6a97d99892e8323b32e3656
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-06 02:34:50 +0800

    Update bismark.bash

--- bismark.bash
+++ bismark.bash
@@ -4,7 +4,7 @@
 for fastq in /home/rtm/methCA/fastq_trim/*R1_val_1.fq.gz;
 do echo $fastq;
 fastq2=$(echo $fastqfile|perl -pe "s/R1_val_1/R2_val_2/g") ;
-echo fastq2;
+echo $fastq2;
 /home/rtm/myprograms/Bismark/bismark --bowtie2 --multicore 22 \
 /home/rtm/resources/hg38_bs_quy/ \
 -1 $fastq \

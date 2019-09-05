b7df14d549fc14fd0f32f8ebd1b5237daa6bb78c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-06 02:34:38 +0800

    Update bismark.bash

--- bismark.bash
+++ bismark.bash
@@ -2,8 +2,9 @@
 # BIsmark align
 
 for fastq in /home/rtm/methCA/fastq_trim/*R1_val_1.fq.gz;
-do ls -lh $fastq; 
+do echo $fastq;
 fastq2=$(echo $fastqfile|perl -pe "s/R1_val_1/R2_val_2/g") ;
+echo fastq2;
 /home/rtm/myprograms/Bismark/bismark --bowtie2 --multicore 22 \
 /home/rtm/resources/hg38_bs_quy/ \
 -1 $fastq \

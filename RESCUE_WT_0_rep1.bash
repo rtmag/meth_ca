5de4e375a26c19d738c410f152e8cc826557b1ee
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-13 15:21:26 +0800

    Update RESCUE_WT_0_rep1.bash

--- RESCUE_WT_0_rep1.bash
+++ RESCUE_WT_0_rep1.bash
@@ -1,1 +1,6 @@
 zcat /home/rtm/methCA/fastq_cat/WT_D0_rep1_R2.fastq.gz|perl -pe 's/CAAACCCTATAAAAAACAATTTAATATCATCTATCAAAATTACAAATACACAAACCATTTAACCCAACAATCCCACATCCAAAAATCTA\@D151BACXX120918\:8\:2111\:21075\:14470/CAAACCCTATAAAAAACAATTTAATATCATCTATCAAAATTACAAATACACAAACCATTTAACCCAACAATCCCACATCCAAAAATCTA\n\@D151BACXX120918\:8\:2111\:21075\:14470/g' >  /home/rtm/methCA/fastq_cat/WT_D0_rep1_R2_rescued.fastq  
+
+/home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 --fastqc \
+-o /home/rtm/methCA/fastq_trim/ \
+/home/rtm/methCA/fastq_cat/WT_D0_rep1_rescued_R1.fastq \
+/home/rtm/methCA/fastq_cat/WT_D0_rep1_rescued_R2.fastq ;

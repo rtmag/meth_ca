0e4b4dc76643af5a78505def6bd4e192762f9991
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-22 06:45:20 +0800

    Update bismark.bash

--- bismark.bash
+++ bismark.bash
@@ -28,3 +28,8 @@ done
 -1 WT_D0_rep1_rescued_R1_val_1.fq \
 -2 WT_D0_rep1_rescued_R2_val_2.fq \
 --output_dir /home/rtm/methCA/bam
+
+/home/rtm/myprograms/Bismark/bismark_methylation_extractor --multicore 20 --gzip \
+--paired-end --ample_memory --comprehensive --cytosine_report \
+--CX_context --genome_folder /home/rtm/resources/hg38_bs_quy/ /home/rtm/methCA/bam/WT_D0_rep1_rescued_R1_val_1_bismark_bt2_pe.bam \
+--output /home/rtm/methCA/bismark_methylation/;

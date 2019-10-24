4ee1545d113987ac706d74fa11b409e05e3e286a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-24 16:13:26 +0800

    Update 1_methylKit.R

--- 1_methylKit.R
+++ 1_methylKit.R
@@ -22,3 +22,23 @@ meth=unite(myobj, destrand=TRUE,mc.cores=22)
 myDiff=calculateDiffMeth(meth,num.cores=40)
 ############################################################################################
 ############################################################################################
+
+file.list=list( 
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep1_CA.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep2_CA.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep1_CA.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep3_CA.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep4_CA.report.txt.gz"
+ )
+
+CAmyobj=methRead(file.list,
+           sample.id=list("D0_1_CA","D0_2_CA","D6_1_CA","D6_3_CA","D6_4_CA"),
+           assembly="hg38",
+           treatment=c(0,0,1,1,1),
+           context="none",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=3)
+           
+meth=unite(myobj, destrand=TRUE,mc.cores=22)
+myDiff=calculateDiffMeth(meth,num.cores=40)

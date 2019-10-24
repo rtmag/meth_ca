94c8906ffbe4c0cba6a3e982110e4e8f16d32e22
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-24 15:39:11 +0800

    Update 1_methylKit.R

--- 1_methylKit.R
+++ 1_methylKit.R
@@ -2,15 +2,15 @@
 library(methylKit)
 
 file.list=list( 
-"/home/rtm/methCA/bismark_methylation/WT_D0_rep1_rescued_R1_val_1_bismark_bt2_pe.CX_report.txt.gz",
-"/home/rtm/methCA/bismark_methylation/WT_D0_rep2_R1_val_1_bismark_bt2_pe.CX_report.txt.gz",
-"/home/rtm/methCA/bismark_methylation/WT_D6_rep1_R1_val_1_bismark_bt2_pe.CX_report.txt.gz",
-"/home/rtm/methCA/bismark_methylation/WT_D6_rep3_R1_val_1_bismark_bt2_pe.CX_report.txt.gz",
-"/home/rtm/methCA/bismark_methylation/WT_D6_rep4_R1_val_1_bismark_bt2_pe.CX_report.txt.gz"
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep1_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep2_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep1_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep3_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep4_CG.report.txt.gz"
  )
 
 myobj=methRead(file.list,
-           sample.id=list("D0","D0","D6","D6","D6"),
+           sample.id=list("D0_1_CG","D0_2_CG","D6_1_CG","D6_3_CG","D6_4_CG"),
            assembly="hg38",
            treatment=c(0,0,1,1,1),
            context="CpG",

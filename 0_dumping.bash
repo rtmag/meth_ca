d41a9cff7eb9e0398ca9ac6568d040319ceede36
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-10-24 14:10:13 +0800

    Update 0_dumping.bash

--- 0_dumping.bash
+++ 0_dumping.bash
@@ -14,3 +14,19 @@ zcat /home/rtm/methCA/bismark_methylation/WT_D0_rep1_rescued_R1_val_1_bismark_bt
  /home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep4_CA.report.txt.gz 
  
  ##
+zcat /home/rtm/methCA/bismark_methylation/WT_D0_rep1_rescued_R1_val_1_bismark_bt2_pe.CX_report.txt.gz|awk '$7 ~ /^CG/' | gzip -c > \
+ /home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep1_CG.report.txt.gz 
+ 
+ zcat /home/rtm/methCA/bismark_methylation/WT_D0_rep2_R1_val_1_bismark_bt2_pe.CX_report.txt.gz|awk '$7 ~ /^CG/' | gzip -c > \
+ /home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep2_CG.report.txt.gz 
+ 
+  zcat /home/rtm/methCA/bismark_methylation/WT_D6_rep1_R1_val_1_bismark_bt2_pe.CX_report.txt.gz|awk '$7 ~ /^CG/' | gzip -c > \
+ /home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep1_CG.report.txt.gz 
+ 
+   zcat /home/rtm/methCA/bismark_methylation/WT_D6_rep3_R1_val_1_bismark_bt2_pe.CX_report.txt.gz|awk '$7 ~ /^CG/' | gzip -c > \
+ /home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep3_CG.report.txt.gz 
+ 
+    zcat /home/rtm/methCA/bismark_methylation/WT_D6_rep4_R1_val_1_bismark_bt2_pe.CX_report.txt.gz|awk '$7 ~ /^CG/' | gzip -c > \
+ /home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep4_CG.report.txt.gz 
+ 
+ #########################

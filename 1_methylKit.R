63e62fe8835e678906b935b57d927bd0c193999e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-11-23 11:02:59 +0800

    Update 1_methylKit.R

--- 1_methylKit.R
+++ 1_methylKit.R
@@ -44,9 +44,10 @@ CAmyobj=methRead(file.list,
 CAmeth=unite(CAmyobj, destrand=FALSE,mc.cores=22)
 CAmyDiff=calculateDiffMeth(CAmeth,num.cores=40)
 saveRDS(CAmyDiff,"D6_vs_D0_CpA_mydiff.rds")
-###
+############################################################################################
+############################################################################################
 
-#pooled CA
+#pooled CG
 library(methylKit)
 
 file.list=list( 
@@ -78,6 +79,40 @@ rm(CAmeth)
 CAmyDiff=calculateDiffMeth(CAmeth_pool,num.cores=40)
 rm(CAmeth_pool)
 saveRDS(CAmyDiff,"D6_vs_D0_CpA_mydiff_fisher_pooled.rds")
+############################################################################################
+############################################################################################
+#pooled CA
+library(methylKit)
+
+file.list=list( 
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep1_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D0_rep2_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep1_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep3_CG.report.txt.gz",
+"/home/rtm/methCA/bismark_methylation/dumps/WT_D6_rep4_CG.report.txt.gz"
+ )
+
+CGmyobj=methRead(file.list,
+           sample.id=list("D0_1_CG","D0_2_CG","D6_1_CG","D6_3_CG","D6_4_CG"),
+           assembly="hg38",
+           treatment=c(0,0,1,1,1),
+           context="none",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=5)
+
+CGmyobj_normalized <- normalizeCoverage(CGmyobj,method="median")
+rm(CGmyobj,file.list)
+CGmeth=unite(CGmyobj_normalized, destrand=TRUE,mc.cores=22,min.per.group=1L)
+dim(CGmeth)
+rm(CGmyobj_normalized)
+CGmeth_pool <- pool(CGmeth,sample.ids=c("D0","D6"))
+dim(CGmeth_pool)
+saveRDS(CGmeth_pool,"CGmeth_d0d6_normalized_united_pooled.rds")
+rm(CGmeth)
+CGmyDiff=calculateDiffMeth(CGmeth_pool,num.cores=40)
+rm(CGmeth_pool)
+saveRDS(CGmyDiff,"D6_vs_D0_CpA_mydiff_fisher_pooled.rds")
 ###
 ###
 ###

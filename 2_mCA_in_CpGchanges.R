9e5fea065c89a94d80faf58afda281c578571250
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-11-26 18:15:28 +0800

    Update 2_mCA_in_CpGchanges.R

--- 2_mCA_in_CpGchanges.R
+++ 2_mCA_in_CpGchanges.R
@@ -1,20 +1,19 @@
 library(liftOver)
-system("wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz")
-system("gunzip hg19ToHg38.over.chain.gz")
+#system("wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz")
+#system("gunzip hg19ToHg38.over.chain.gz")
 dmr <- read.csv("mmc3_DMR.csv")
 
-hg19Tohg38 <- function(peaks)
-{
-  peaks <- GRanges(peak_ori[,1],IRanges(peak_ori[,2], peak_ori[,3]))
+peaksori = dmr[,1:3]
+
+  peaks <- GRanges(peaksori[,1],IRanges(peaksori[,2], peaksori[,3]))
   ch <- import.chain("hg19ToHg38.over.chain")
-  cur19 <- suppressWarnings(rtracklayer::liftOver(peaks, ch))
+  cur19 <- rtracklayer::liftOver(peaks, ch)
   cur19 <- unlist(cur19)
   GenomeInfoDb::genome(cur19) <- "hg38"
   cur19_df <- data.frame(cur19)
-  return(cur19_df[,c("seqnames","start","end")])
-}
+  dmrhg38 <- cur19_df[,c("seqnames","start","end")]
 
-dmrhg38<- hg19Tohg38(dmr[,1:3])
+
 dmr[,1] <- dmrhg38[,1]
 dmr[,2] <- dmrhg38[,2]
 dmr[,3] <- dmrhg38[,3]
